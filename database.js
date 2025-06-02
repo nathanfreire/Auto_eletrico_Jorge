db43fb7f726bcf16a8d9adda5ba316a8b362f224
Nathan Freire <nathan.freire1403@gmail.com>
2025-06-02 16:42:51 -0300

    finalização e publicação

--- database.js
+++ database.js
@@ -1,28 +1,14 @@
-/**
- * Módulo e conexão com o banco de dados uso do framework mongoose
- */
-
-
 const { connect } = require('http2')
 const mongoose = require('mongoose')
 
 
-
-
-
-const url = 'mongodb+srv://admin:123Senac@projetonode.4bgbm.mongodb.net/dbAuto_eletrico_jorge'
-
-
+const url = 'mongodb://localhost:27017/'
 
 let conectado = false
-
-
-
 
 const conectar = async() => {
     
     if (!conectado) {
-        
         
         try {
             await mongoose.connect(url) 
